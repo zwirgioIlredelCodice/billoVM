97d143e6579a04176c019537ca495725f7af7fe1
Fabio Murer <murer.fabio23@gmail.com>
2021-10-17 17:58:57 +0200

    sus

--- ht.c
+++ ht.c
@@ -46,8 +46,32 @@ int ht_get(ht *table, size_t key)
     return 0;
 }
 
+void ht_expand(ht *table, size_t capacity)
+{
+    ht_cell *new_cell = malloc(sizeof(ht_cell) * capacity);
+
+    for (size_t i = 0; i < capacity; i++)
+    {
+        new_cell[i].empty = true;
+    }
+    for (size_t i = 0; i < table->capacity; i++)
+    {
+        ht_cell cell = table->cell[i];
+        if (cell.key != NULL)
+        {
+            ht_set(new, new_capacity, entry.key,
+                         entry.value, NULL);
+        }
+    }
+}
+
 void ht_set(ht *table, size_t key, int value)
 {
+    // If length will exceed 3/4 of current capacity, expand it.
+    if (table->length >= (table->capacity / 4) * 3)
+    {
+        ht_resize(table, (table->capacity / 4) * 3);
+    }
     size_t hash_num = hash(key, table->capacity);
     size_t index = hash_num;
 
@@ -68,6 +92,7 @@ void ht_set(ht *table, size_t key, int value)
     }
 
     // Didn't find key, allocate+copy if needed, then insert it.
+    table->length++;
     table->cell[index].empty = false;
     table->cell[index].key = key;
     table->cell[index].value = value;
@@ -75,6 +100,12 @@ void ht_set(ht *table, size_t key, int value)
 
 void ht_delate(ht *table, size_t key)
 {
+    // If length is less than 1/2 of current capacity, reduce it.
+    if (table->length <= table->capacity / 2)
+    {
+        ht_resize(table, table->capacity / 2);
+    }
+
     size_t hash_num = hash(key, table->capacity);
     size_t index = hash_num;
 
@@ -83,6 +114,7 @@ void ht_delate(ht *table, size_t key)
         if (key == table->cell[index].key)
         {
             // Found key, delate value.
+            table->length--;
             table->cell[index].value = 0;
             table->cell[index].empty = true;
         }
@@ -100,7 +132,7 @@ int main()
 {
     ht table;
     ht_create(&table);
-    ht_set(&table,2,55);
+    ht_set(&table, 2, 55);
     int viva = ht_get(&table, 2);
     printf("%d", viva);
     ht_destroy(&table);
