7279ed76fde4d923d908190c7969941084661494
Fabio Murer <murer.fabio23@gmail.com>
2021-10-18 18:26:59 +0200

    error when ht_resize ht_set_table infinite loop

--- ht.c
+++ ht.c
@@ -46,23 +46,54 @@ int ht_get(ht *table, size_t key)
     return 0;
 }
 
-void ht_expand(ht *table, size_t capacity)
+void ht_resize(ht *table, size_t new_capacity)
 {
-    ht_cell *new_cell = malloc(sizeof(ht_cell) * capacity);
+    ht_cell *new_cell = malloc(sizeof(ht_cell) * new_capacity);
 
-    for (size_t i = 0; i < capacity; i++)
+    for (size_t i = 0; i < new_capacity; i++)
     {
         new_cell[i].empty = true;
     }
     for (size_t i = 0; i < table->capacity; i++)
     {
         ht_cell cell = table->cell[i];
-        if (cell.key != NULL)
+    
+        if (!cell.empty)
         {
-            ht_set(new, new_capacity, entry.key,
-                         entry.value, NULL);
+            ht_set_table(new_cell, cell.key, cell.value, new_capacity);
         }
     }
+    // Free old entries array and update this table's details.
+    free(table->cell);
+    table->cell = new_cell;
+    table->capacity = new_capacity;
+}
+
+void ht_set_table(ht_cell *cell, size_t key, int value, size_t capacity)
+{
+    size_t hash_num = hash(key, capacity);
+    size_t index = hash_num;
+
+    while (!cell[index].empty)
+    {
+        if (key == cell[index].key)
+        {
+            // Found key (it already exists), update value.
+            cell[index].value = value;
+        }
+        // Key wasn't in this slot, move to next (linear probing).
+        index++;
+        if (index >= capacity)
+        {
+            // At end of entries array, wrap around.
+            index = 0;
+        }
+    }
+
+    // Didn't find key, allocate+copy if needed, then insert it.
+    cell[index].empty = false;
+    cell[index].key = key;
+    cell[index].value = value;
 }
 
 void ht_set(ht *table, size_t key, int value)
@@ -72,30 +103,9 @@ void ht_set(ht *table, size_t key, int value)
     {
         ht_resize(table, (table->capacity / 4) * 3);
     }
-    size_t hash_num = hash(key, table->capacity);
-    size_t index = hash_num;
 
-    while (!table->cell[index].empty)
-    {
-        if (key == table->cell[index].key)
-        {
-            // Found key (it already exists), update value.
-            table->cell[index].value = value;
-        }
-        // Key wasn't in this slot, move to next (linear probing).
-        index++;
-        if (index >= table->capacity)
-        {
-            // At end of entries array, wrap around.
-            index = 0;
-        }
-    }
-
-    // Didn't find key, allocate+copy if needed, then insert it.
+    ht_set_table(table->cell, key, value, table->capacity);
     table->length++;
-    table->cell[index].empty = false;
-    table->cell[index].key = key;
-    table->cell[index].value = value;
 }
 
 void ht_delate(ht *table, size_t key)
@@ -128,12 +138,32 @@ void ht_delate(ht *table, size_t key)
     }
 }
 
+void ht_show(ht *table)
+{
+    printf("elements=%d\n", table->length);
+    for (size_t i = 0; i < table->capacity; i++)
+    {
+        if (table->cell[i].empty)
+        {
+            printf("empty\n");
+        }
+        else
+        {
+            printf("slot %d key=%d value=%d\n", i, table->cell[i].key, table->cell[i].value);
+        }
+    }
+    printf("\n");
+}
+
 int main()
 {
     ht table;
     ht_create(&table);
-    ht_set(&table, 2, 55);
-    int viva = ht_get(&table, 2);
-    printf("%d", viva);
+    ht_set(&table, 2, 54);
+    ht_set(&table, 3, 53);
+    ht_set(&table, 4, 52);
+    ht_show(&table);
+    ht_set(&table, 5, 51);
+    ht_show(&table);
     ht_destroy(&table);
 }
--- ht.h
+++ ht.h
@@ -24,6 +24,12 @@ void ht_destroy(ht* table);
 
 int ht_get(ht* table, size_t key);
 
+void ht_resize(ht *table, size_t new_capacity);
+
+void ht_set_table(ht_cell *cell, size_t key, int value, size_t capacity);
+
 void ht_set(ht* table, size_t key, int value);
 
 void ht_delate(ht* table, size_t key);
+
+void ht_show(ht *table);
