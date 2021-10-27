f62f0b01db7e683f8d39942900d8dd6a746f1c4f
Fabio Murer <murer.fabio23@gmail.com>
2021-10-27 18:27:50 +0200

    start sly

--- billolang.py
+++ billolang.py
@@ -1,33 +1,68 @@
-def read_file(file):
-    f  = open(file, "r")
-    data = f.read()
-    f.close()
-    return data
+# calclex.py
 
-keyword = ["function", "return", "print"]
-newline = ["->", "-->"]
-operation = ["=", "+", "-", "*", "/"]
-block_open = "("
-block_close = ")"
+from sly import Lexer
 
-data = read_file("one.billo")
-data_splitted = data.split()
+class CalcLexer(Lexer):
+    # Set of token names.   This is always required
+    tokens = { NUMBER, ID, WHILE, FOR, IF, ELSE, PRINT, VAR,
+               PLUS, MINUS, TIMES, DIVIDE, ASSIGN,
+               EQ, LT, LE, GT, GE, NE, ARROW, ARROWL }
 
-data_token_type = []
-for token in data_splitted:
-    if token in keyword:
-        data_token_type.append("t_keyword")
-    elif token in newline:
-        data_token_type.append("t_newline")
-    elif token in operation:
-        data_token_type.append("t_operation")
-    elif token == block_open:
-        data_token_type.append("t_block_open")
-    elif token == block_close:
-        data_token_type.append("t_block_close")
-    else:
-        data_token_type.append("t_?")
 
-print(data)
-for i in range(len(data_splitted)):
-    print("token= ", data_splitted[i], "type= ",data_token_type[i])
+    literals = { '(', ')'}
+
+    # String containing ignored characters
+    ignore = ' \t'
+
+    # Regular expression rules for tokens
+    ARROW   = r'->'
+    ARROWL  = r'-->'
+    PLUS    = r'\+'
+    MINUS   = r'-'
+    TIMES   = r'\*'
+    DIVIDE  = r'/'
+    EQ      = r'=='
+    ASSIGN  = r'='
+    LE      = r'<='
+    LT      = r'<'
+    GE      = r'>='
+    GT      = r'>'
+    NE      = r'!='
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    # Identifiers and keywords
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['while'] = WHILE
+    ID['for'] = FOR
+    ID['print'] = PRINT
+    ID['var'] = VAR
+
+    ignore_comment = r'\#.*'
+
+    # Line number tracking
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        self.index += 1
+
+if __name__ == '__main__':
+    data = '''# Counting
+var x ->
+= x 0 ->
+( while < x 10 -->
+    print x ->
+    = x (+ x 1) ->
+)
+'''
+    lexer = CalcLexer()
+    for tok in lexer.tokenize(data):
+        print(tok)
