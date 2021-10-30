11f28ec5e093b5c85ddb347c2871119446c96e51
Fabio Murer <murer.fabio23@gmail.com>
2021-10-30 16:35:22 +0200

    passo in dietro

--- billolang.py
+++ billolang.py
@@ -1,33 +1,72 @@
+
 def read_file(file):
-    f  = open(file, "r")
+    f = open(file, "r")
     data = f.read()
     f.close()
     return data
 
-keyword = ["function", "return", "print"]
-newline = ["->", "-->"]
-operation = ["=", "+", "-", "*", "/"]
-block_open = "("
-block_close = ")"
 
+class token:
+    def __init__(self, kind, value):
+        self.kind = kind
+        self.value = value
+
+
+def print_token(token_list):
+    for i in range(len(token_list)):
+        print("type", token_list[i].kind, "value ", token_list[i].value)
+
+
+def tokenize(data):
+    token_list = []
+
+    keyword = ["var", "if"]
+    operation = ["=", "+", "-", "*", "/"]
+    arrow = "->"
+
+    # split string into token when "\n" " " "\t"
+    token_value = data.split()
+
+    # set class token value
+    for tok in token_value:
+        token_list.append(token("", tok))
+
+
+    # find what token is p1
+    for i in token_list:
+
+        if i.value.isdigit():
+            i.kind = "t_number"
+
+        elif i.value in keyword:
+            i.kind = "t_keyword"
+
+        elif i.value == arrow:
+            i.kind = "t_arrow"
+
+        elif i.value in operation:
+            i.kind = "t_operation"
+
+        else:
+            i.kind = "t_?"
+
+    # find what token is p2 recognise variable
+        number_token = len(token_list)
+
+    for i in range(number_token):
+        if token_list[i].kind == "t_keyword" and token_list[i].value == "var":
+            new_variable = token_list[i+1].value  # variable name
+
+            # change all token after with type t_? with t_variable if is recognised t_variable = new_variable
+            for y in range(i+1, number_token):
+                if token_list[y].kind == "t_?" and token_list[y].value == new_variable:
+                    token_list[y].kind = "t_variable"
+    
+    return token_list
+
+
+# read from file
 data = read_file("one.billo")
-data_splitted = data.split()
+token_list = tokenize(data)
 
-data_token_type = []
-for token in data_splitted:
-    if token in keyword:
-        data_token_type.append("t_keyword")
-    elif token in newline:
-        data_token_type.append("t_newline")
-    elif token in operation:
-        data_token_type.append("t_operation")
-    elif token == block_open:
-        data_token_type.append("t_block_open")
-    elif token == block_close:
-        data_token_type.append("t_block_close")
-    else:
-        data_token_type.append("t_?")
-
-print(data)
-for i in range(len(data_splitted)):
-    print("token= ", data_splitted[i], "type= ",data_token_type[i])
+print_token(token_list)
