e9d1471b2e8455c481d4b465f16042f8d7160d4c
Fabio Murer <murer.fabio23@gmail.com>
2021-11-05 16:47:02 +0100

    add command line arguments and more

--- billolang.py
+++ billolang.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # if not working chmod +x billolang.py
 
+import sys
+
 #           DECLARING TERMINAL
 #           COLOR 
 RED   = "\033[1;31m"  
@@ -79,6 +81,14 @@ def read_file(file):
     f.close()
     return data
 
+def print_byte_file(file, bytecode):
+    f = open(file, "wb")
+    f.write(bytecode)
+    f.close()
+
+
+# given a list of token, print them
+# 
 def print_token(token_list_line):
     line_n = 0
     for line_tok in token_list_line:
@@ -89,36 +99,30 @@ def print_token(token_list_line):
 
 def tokenize(data):
 
-    error = 0
-
-    token_list = []
-
     # split string into token when "\n" " " "\t"
     token_value = data.split()
 
-    # set class token value
-    for tok in token_value:
-        token_list.append(token("", tok))
+    token_list = []
+    kind  = ""
+    
+    for value in token_value:
+        if value.isdigit():
+            kind = "t_number"
 
+        elif value in keyword:
+            kind = "t_keyword"
 
-    # find what token is p1
-    for i in token_list:
+        elif value == arrow:
+            kind = "t_arrow"
 
-        if i.value.isdigit():
-            i.kind = "t_number"
-
-        elif i.value in keyword:
-            i.kind = "t_keyword"
-
-        elif i.value == arrow:
-            i.kind = "t_arrow"
-
-        elif i.value in operation:
-            i.kind = "t_operation"
+        elif value in operation:
+            kind = "t_operation"
 
         else:
-            i.kind = "t_?"
-
+            kind = "t_?"
+        
+        token_list.append(token(kind, value)) #add new token to token_list[]
+        
     # p2 make list line
 
     token_list_line = []
@@ -126,7 +130,7 @@ def tokenize(data):
 
     for tok in token_list:
 
-        if tok.kind != "t_arrow":
+        if tok.kind != "t_arrow": # when -> end line
             token_line.append(tok)
 
         else:
@@ -142,7 +146,8 @@ def instruction_str_to_n(program):
 
     for i in range(len(program)):
 
-        #for opcode
+        # like switch case for all of opcodes
+        # translate opcode name(string) to a opcode value(number)
         if program[i].opcode == "LOAD_C":
             opcode = LOAD_C
         elif program[i].opcode == "ADDITION_C":
@@ -194,21 +199,23 @@ def instruction_str_to_n(program):
         elif program[i].opcode == "PRINT_MEMORY":
             opcode = PRINT_MEMORY
         else:
-            opcode = 0
-            print("asasdasd",program[i].opcode)
+            print("not found opcode",program[i].opcode)
+            exit(1)
         
-        #for operand
+        # check operand validity and set value for jumps 
         if program[i].operand.startswith("o@") or program[i].operand.startswith("c@"):
             for ii in range(len(program)):
                 if program[i].operand in program[ii].point_control_flow:
                     operand = ii
         else:
-            operand = program[i].operand
+            operand = int(program[i].operand)
         
         program_n.append(instruction_n(opcode, operand))
 
     return program_n
 
+# given array of line of token
+#
 def traduce(token_list_line):
 
     control_flow_stack = [] #stack containing if, for, while statement open
@@ -240,41 +247,33 @@ def traduce(token_list_line):
                 opcode = "NOT_C"
             for tok in line_tok[1:]:
                 program.append(Instruction(opcode, tok.value))
-                print(instruction_n, opcode, tok.value)
 
                 instruction_n += 1
 
         elif keyword_startline == "if":
 
             control_flow_stack.append(control_flow("if", instruction_n))
-            print(RED,"\t=OPEN= statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
             program[-1].point_control_flow.append("o@"+str(control_flow_stack[-1].position))
 
         elif keyword_startline == "while":
 
             control_flow_stack.append(control_flow("while", instruction_n))
-            print(RED,"\t=OPEN= statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
             program[-1].point_control_flow.append("o@"+str(control_flow_stack[-1].position))
 
         elif keyword_startline == "do":
-            print(GREEN,"\tif register is false jump *CLOSE* statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
 
             opcode = "JUMP_CONDITIONAL"
             operand = "c@"+str(control_flow_stack[-1].position) # is not right todo: after make code all instruction change @.. value with right ones
-            print(GREEN,instruction_n, opcode, operand, RESET)
             program.append(Instruction(opcode, operand))
             instruction_n += 1
     
         elif keyword_startline == "end":
 
                 if control_flow_stack[-1].kind == "while":
-                    print(GREEN,"\tjump *OPEN* statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
                     opcode = "JUMP"
                     operand = "o@"+str(control_flow_stack[-1].position)
-                    print(GREEN,instruction_n, opcode, operand, RESET)
                     program.append(Instruction(opcode, operand))
                     instruction_n += 1
-                    print(RED,"\t=CLOSE= statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
                     program[-1].point_control_flow.append("c@"+str(control_flow_stack[-1].position))
 
                     control_flow_stack.pop()
@@ -284,19 +283,14 @@ def traduce(token_list_line):
                     control_flow_temp = control_flow_stack.pop()
 
                     control_flow_stack.append(control_flow("else", instruction_n))
-                    print(GREEN,"\tjump *CLOSE* statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
                     opcode = "JUMP"
                     operand = "c@"+str(control_flow_stack[-1].position)
-                    print(GREEN,instruction_n, opcode, operand, RESET)
                     program.append(Instruction(opcode, operand))
                     instruction_n += 1
-                    print(RED,"\t=CLOSE= statement",control_flow_temp.position, "type",control_flow_temp.kind ,RESET)
                     program[-1].point_control_flow.append("c@"+str(control_flow_stack[-1].position))
-                    print(RED,"\t=OPEN= statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
                     program[-1].point_control_flow.append("o@"+str(control_flow_stack[-1].position))
                     
                 else:
-                    print(RED,"\t=CLOSE= statement",control_flow_stack[-1].position, "type",control_flow_stack[-1].kind ,RESET)
                     program[-1].point_control_flow.append("c@"+str(control_flow_stack[-1].position))
                     control_flow_stack.pop()
 
@@ -305,21 +299,9 @@ def traduce(token_list_line):
             opcode = "LOAD_C"
             for tok in line_tok:
                 program.append(Instruction(opcode, tok.value))
-                print(instruction_n, opcode, tok.value)
                 instruction_n += 1
-    
-    i = 0
-    for instruction in program:
-        print(i, instruction.opcode, instruction.operand, 
-            instruction.point_control_flow,)
-        i += 1
-    
-    program_n = instruction_str_to_n(program)
-
-    i = 0
-    for instruction in program_n:
-        print(i, instruction.opcode, instruction.operand)
-        i += 1
+    return program
+        
 
 def to_bytecode(code):
     size_int = 2 # 2 byte make a c int
@@ -330,28 +312,18 @@ def to_bytecode(code):
         b_operand = instruction.operand.to_bytes(size_int, byteorder='big', signed=True)
         bytecode += b_opcode
         bytecode += b_operand
-    print(bytecode)
+    return bytecode
 
-    f = open("out.bbillo", "wb")
-    f.write(bytecode)
-    f.close()
-
-# read from file
-name_file = "one.billo"
-data = read_file(name_file)
-if data != "": #if is not empty
-    token_list_line = tokenize(data)
-    print_token(token_list_line)
-
-    if token_list_line != -1: #if tokenize() return no errors
-        print("\nTRADUCE\n")
-        traduce(token_list_line)
-
+if __name__ == "__main__":
+    cli_arguments = sys.argv[1:] # the first argument is the mane of the script so delate it
+    if len(cli_arguments) > 0:
+        file_in = cli_arguments[0]
+        file_out = cli_arguments[1]
+        data = read_file(file_in)
+        token_list_line = tokenize(data)
+        instruction = traduce(token_list_line)
+        instruction_n = instruction_str_to_n(instruction)
+        bytecode = to_bytecode(instruction_n)
+        print_byte_file(file_out, bytecode)
     else:
-        print("error EXIT")
-
-else:
-    print("file ", name_file, "empty EXIT")
-
-#program = [Instruction(2, 234),Instruction(2, 234),Instruction(2, 234)]
-#to_bytecode(program)
+        print("file name argument is missing -> EXIT")
